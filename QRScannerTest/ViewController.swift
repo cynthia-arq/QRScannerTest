e584ef8b8fea6b3dac5df51b4cde73a146f36e8d
cynthia fu <cynthia.fu@outware.com.au>
2019-03-26 09:23:43 +1000

    init

---
+++ QRScannerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScannerTest
+//
+//  Created by cynthia fu on 26/3/19.
+//  Copyright © 2019 cynthia fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
